f8c9fa1641d72046979cf26b9f9dba1c85904b8f
MatthewWhiting <MatthewWhiting@csiro.au>
2015-09-09 05:18:18 +0000

    ASKAPSDP-1682 - Adding missing RMData classes

---
+++ Code/Components/Analysis/analysis/current/polarisation/RMData.cc
@@ -0,0 +1,145 @@
+/// @file
+///
+/// Implementation of RM data measurement
+///
+/// @copyright (c) 2014 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Matthew Whiting <Matthew.Whiting@csiro.au>
+///
+#include <polarisation/RMdata.h>
+#include <askap_analysis.h>
+#include <polarisation/RMSynthesis.h>
+
+#include <askap/AskapLogging.h>
+#include <askap/AskapError.h>
+
+#include <casa/Arrays/Vector.h>
+#include <casa/Arrays/Matrix.h>
+#include <casa/Arrays/ArrayMath.h>
+#include <complex>
+#include <casa/BasicSL/Complex.h>
+
+#include <Common/ParameterSet.h>
+
+///@brief Where the log messages go.
+ASKAP_LOGGER(logger, ".rmdata");
+
+namespace askap {
+
+namespace analysis {
+
+static const float defaultSNRthreshold = 8.0;
+static const float defaultDebiasThreshold = 5.0;
+
+RMData::RMData(const LOFAR::ParameterSet &parset):
+    itsDetectionThreshold(parset.getFloat("polThresholdSNR", defaultSNRthreshold)),
+    itsDebiasThreshold(parset.getFloat("polThresholdDebias", defaultDebiasThreshold))
+{
+
+}
+
+void RMData::calculate(RMSynthesis *rmsynth)
+{
+    const casa::Vector<casa::Complex> fdf = rmsynth->fdf();
+    casa::Vector<float> fdf_p = casa::amplitude(fdf);
+    const casa::Vector<float> phi_rmsynth = rmsynth->phi();
+    const float noise = rmsynth->fdf_noise();
+    const float RMSF_FWHM = rmsynth->rmsf_width();
+    const float lsqzero = rmsynth->refLambdaSq();
+    const unsigned int numChan = rmsynth->numFreqChan();
+
+    float minFDF, maxFDF;
+    casa::IPosition locMin, locMax;
+    casa::minMax<float>(minFDF, maxFDF, locMin, locMax, fdf_p);
+
+    itsSNR = maxFDF / noise;
+    itsFlagDetection = (itsSNR > itsDetectionThreshold);
+
+    if (itsFlagDetection) {
+
+        itsPintPeak = maxFDF;
+        itsPintPeak_err = M_SQRT2 * fabs(itsPintPeak) / noise;
+        itsPintPeakEff = sqrt(maxFDF * maxFDF - 2.3 * noise);
+
+        itsPhiPeak = phi_rmsynth(locMax);
+        itsPhiPeak_err = RMSF_FWHM * noise / (2. * itsPintPeak);
+
+
+//      itsPolAngleRef = 0.5 * casa::phase(fdf(locMax));  // THIS MAY NOT WORK - NEED TO CHECK
+        itsPolAngleRef_err = 0.5 * noise / fabs(itsPintPeak);
+
+        itsPolAngleZero = itsPolAngleRef - itsPhiPeak * lsqzero;
+        itsPolAngleZero_err = (1. / (4.*(numChan - 2) * itsPintPeak * itsPintPeak)) *
+                              (float((numChan - 1) / numChan) + pow(lsqzero, 4) /
+                               rmsynth->lsqVariance());
+
+//      itsFracPol
+        // Need to get the Imodel into the RMSynth object somehow.
+
+    } else {
+
+        //itsPintPeak = maxFDF * itsDetectionThreshold;
+        itsPintPeak = noise * itsDetectionThreshold;
+        itsPintPeakEff = -1.;
+
+
+
+
+    }
+
+}
+
+//----------------------------------------//
+
+void RMData::printSummary()
+{
+    if (itsFlagDetection) {
+
+        std::cout << "Detected!" << std::endl;
+        std::cout << "Peak Polarised intensity = "
+                  << itsPintPeak << std::endl;
+        std::cout << "Peak Polarised intensity (error) = "
+                  << itsPintPeak_err << std::endl;
+        std::cout << "RM of Peak Polarised intensity = "
+                  << itsPhiPeak << std::endl;
+        std::cout << "RM of Peak Polarised intensity (error) = "
+                  << itsPhiPeak_err << std::endl;
+        std::cout << "Peak Polarised intensity (effective) = "
+                  << itsPintPeakEff << std::endl;
+
+    } else {
+
+        std::cout << "Not detected." << std::endl;
+        std::cout << "Limit on peak polarised intensity = "
+                  << itsPintPeak << std::endl;
+
+    }
+
+
+
+
+}
+
+
+}
+}
---
+++ Code/Components/Analysis/analysis/current/polarisation/RMData.h
@@ -0,0 +1,108 @@
+/// @file
+///
+/// Holds the measured parameters taken from the results of RM Synthesis
+///
+/// @copyright (c) 2014 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Matthew Whiting <Matthew.Whiting@csiro.au>
+///
+#ifndef ASKAP_ANALYSIS_RM_DATA_H_
+#define ASKAP_ANALYSIS_RM_DATA_H_
+#include <polarisation/RMSynthesis.h>
+#include <Common/ParameterSet.h>
+
+namespace askap {
+
+namespace analysis {
+
+class RMData {
+    public:
+        RMData(const LOFAR::ParameterSet &parset);
+        virtual ~RMData() {};
+
+        void calculate(RMSynthesis *rmsynth);
+
+        void printSummary();
+
+        const float detectionThreshold() { return itsDetectionThreshold;};
+        const float debiasThreshold() { return itsDebiasThreshold;};
+        const float pintPeak() {return itsPintPeak;};
+        const float pintPeak_err() {return itsPintPeak_err;};
+        const float pintPeakEff() {return itsPintPeakEff;};
+        const float phiPeak() {return itsPhiPeak;};
+        const float phiPeak_err() {return itsPhiPeak_err;};
+        const float pintPeakFit() {return itsPintPeakFit;};
+        const float pintPeakFit_err() {return itsPintPeakFit_err;};
+        const float pintPeakFitEff() {return itsPintPeakFitEff;};
+        const float phiPeakFit() {return itsPhiPeakFit;};
+        const float phiPeakFit_err() {return itsPhiPeakFit_err;};
+        const bool flagDetection() {return itsFlagDetection;};
+        const bool flagEdge() {return itsFlagEdge;};
+        const float polAngleRef() {return itsPolAngleRef;};
+        const float polAngleRef_err() {return itsPolAngleRef_err;};
+        const float polAngleZero() {return itsPolAngleZero;};
+        const float polAngleZero_err() {return itsPolAngleZero_err;};
+        const float fracPol() {return itsFracPol;};
+        const float fracPol_err() {return itsFracPol_err;};
+        const float sNR() {return itsSNR;};
+        const float sNR_err() {return itsSNR_err;};
+
+    private:
+
+        float itsDetectionThreshold;
+        float itsDebiasThreshold;
+
+        float itsPintPeak;
+        float itsPintPeak_err;
+        float itsPintPeakEff;
+        float itsPhiPeak;
+        float itsPhiPeak_err;
+        float itsPintPeakFit;
+        float itsPintPeakFit_err;
+        float itsPintPeakFitEff;
+        float itsPhiPeakFit;
+        float itsPhiPeakFit_err;
+
+        bool itsFlagDetection;
+        bool itsFlagEdge;
+
+        float itsPolAngleRef;
+        float itsPolAngleRef_err;
+        float itsPolAngleZero;
+        float itsPolAngleZero_err;
+
+        float itsFracPol;
+        float itsFracPol_err;
+
+        float itsSNR;
+        float itsSNR_err;
+
+
+
+};
+
+}
+
+}
+
+#endif
