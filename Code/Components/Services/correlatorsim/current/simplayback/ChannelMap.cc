e380fb9286f80eb02a258c86d3983db25e804449
PaulusLahur <PaulusLahur@csiro.au>
2015-09-28 02:00:15 +0000

    https://jira.csiro.au/browse/ASKAPSDP-1680 Added Channel mapping to / from correlator (following the code from Max)

---
+++ Code/Components/Services/correlatorsim/current/simplayback/ChannelMap.cc
@@ -0,0 +1,69 @@
+/// @file ChannelMap.cc
+///
+/// @copyright (c) 2010 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Paulus Lahur <paulus.lahur@csiro.au>
+
+
+// Include own header file first
+#include "ChannelMap.h"
+
+// System includes
+#include <iostream>
+#include <stdint.h>
+
+#include "askap/AskapError.h"
+
+using namespace std;
+
+using namespace askap;
+using namespace askap::cp;
+
+
+ChannelMap::ChannelMap () {;}
+
+
+ChannelMap::~ChannelMap() {;}
+
+
+// Copied from Max
+
+uint32_t ChannelMap::fromCorrelator (uint32_t channelID) {
+	ASKAPDEBUGASSERT(channelID < 216);
+	const uint32_t fineOffset = channelID % 9;
+	const uint32_t group = channelID / 9;
+	//ASKAPDEBUGASSERT(group < 24);
+	const uint32_t chip = group / 4; 
+	const uint32_t coarseChannel = group % 4;
+	return fineOffset + chip * 9 + coarseChannel * 54;
+}
+
+
+uint32_t ChannelMap::toCorrelator (uint32_t channelID) {
+	ASKAPDEBUGASSERT(channelID < 216);
+	const uint32_t fineOffset = channelID % 9;
+	const uint32_t group = channelID / 9;
+	const uint32_t chip = group % 6;
+	const uint32_t coarseChannel = group / 6;
+	return fineOffset + 9 * (coarseChannel + chip * 4);
+}
---
+++ Code/Components/Services/correlatorsim/current/simplayback/ChannelMap.h
@@ -0,0 +1,54 @@
+/// @file Permutation.h
+///
+/// @copyright (c) 2010 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Paulus Lahur <paulus.lahur@csiro.au>
+///
+/// Map channel ID from and to correlator.
+/// Note that the channels are not contiguous in the correlator.
+
+#ifndef ASKAP_CP_CHANNELMAP_H
+#define ASKAP_CP_CHANNELMAP_H
+
+#include <stdint.h>
+
+namespace askap {
+namespace cp {
+
+class ChannelMap {
+	
+	public :
+	
+		ChannelMap ();
+		
+		virtual ~ChannelMap ();
+
+		uint32_t fromCorrelator (uint32_t channelID);
+		
+		uint32_t toCorrelator (uint32_t channelID);
+};
+ 
+};
+};
+
+#endif
