213b6d76db6032e8e964dcba3e1e79f4cf326d67
PaulusLahur <PaulusLahur@csiro.au>
2015-10-09 06:20:40 +0000

    https://jira.csiro.au/browse/ASKAPSDP-1680 Changed the content of playback.in to allow for setting delay time in sending data from the file

--- Code/Components/Services/correlatorsim/current/functests/test_playbackADE/run.sh
+++ Code/Components/Services/correlatorsim/current/functests/test_playbackADE/run.sh
@@ -8,6 +8,7 @@ if [ ! -f ../../init_package_env.sh ]; then
     exit 1
 fi
 source ../../init_package_env.sh
+export AIPSPATH=$ASKAP_ROOT/Code/Base/accessors/current
 
 # Start the Ice Services
 ../start_ice.sh ../iceregistry.cfg ../icegridadmin.cfg ../icestorm.cfg
@@ -17,16 +18,21 @@ if [ $? -ne 0 ]; then
 fi
 sleep 1
 
-# Start the metadata subscriber (don't use the script so this script can kill it)
+# Start the metadata subscriber 
+# (don't use the script so this script can kill it)
 ../../apps/msnoop -c msnoop.in -v > msnoop.log 2>&1 &
 MDPID=$!
 
-# Start the visibilities receiver (don't use the script so this script can kill it)
+# Start the visibilities receiver 
+# (don't use the script so this script can kill it)
 ../../apps/vsnoopADE -v -p 3001 > vsnoop1.log 2>&1 &
 VISPID1=$!
 
 # Run the test
-mpirun -np 3 ../../apps/playbackADE.sh -c playback.in
+# For the moment, keep to 2 processes:
+# process 0 for configuration validation
+# process 1 for streaming
+mpirun -np 2 ../../apps/playbackADE.sh -c playback.in
 STATUS=$?
 
 # Give the subscriber a moment to get the last messages then exit
