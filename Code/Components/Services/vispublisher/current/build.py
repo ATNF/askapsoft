e1d1558b44db1091f39b858700273a84da23fa52
MaximVoronkov <MaximVoronkov@csiro.au>
2016-01-07 06:31:02 +0000

    added deployment-specific files for vispublisher and adjusted build script to install them (ASKAPSDP-1713)

--- Code/Components/Services/vispublisher/current/build.py
+++ Code/Components/Services/vispublisher/current/build.py
@@ -1,6 +1,13 @@
 # @file
 # build script for AutoBuild
 from askapdev.rbuild.builders import Scons as Builder
+from askapdev.rbuild.builders import Data as DataBuilder
 
+# C++ stuff
 b = Builder(".")
 b.build()
+
+# deployment-specific files, i.e. start scripts
+databuilder = DataBuilder("scripts")
+databuilder.build()
+
