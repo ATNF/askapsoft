e200a8d89ae5b17e8a208ccd81550e9a23c8b06a
MaximVoronkov <MaximVoronkov@csiro.au>
2016-01-19 02:57:51 +0000

    added unit tests to cover the new functionality for rank allocation (ASKAPSDP-1605)

--- Code/Base/parallelframework/current/tests/processingsteps/CompositeStepTest.h
+++ Code/Base/parallelframework/current/tests/processingsteps/CompositeStepTest.h
@@ -44,9 +44,13 @@ class CompositeStepTest : public CppUnit::TestFixture
 {
    CPPUNIT_TEST_SUITE(CompositeStepTest);
    CPPUNIT_TEST(testAddSubStep);
+   CPPUNIT_TEST(testAddSubStepFlexMultiRank);
+   CPPUNIT_TEST_EXCEPTION(testAddSubStepTwoFlex, AskapError);
    CPPUNIT_TEST_SUITE_END();
 public:
    void testAddSubStep();   
+   void testAddSubStepFlexMultiRank();   
+   void testAddSubStepTwoFlex();   
 };
 
 void CompositeStepTest::testAddSubStep() {
@@ -59,6 +63,94 @@ void CompositeStepTest::testAddSubStep() {
    CPPUNIT_ASSERT_EQUAL(0, cs.itsSteps[0].id().first());
    CPPUNIT_ASSERT_EQUAL(0, cs.itsSteps[0].id().last());
    CPPUNIT_ASSERT_EQUAL(1u, cs.itsSteps[0].id().nRanks());
+
+   // add 3 groups of 2 ranks each
+   cs.addSubStep(ps, 2, 3);
+   CPPUNIT_ASSERT_EQUAL(size_t(2u), cs.itsSteps.size());
+   CPPUNIT_ASSERT_EQUAL(1, cs.itsSteps[1].id().first());
+   CPPUNIT_ASSERT_EQUAL(6, cs.itsSteps[1].id().last());
+   CPPUNIT_ASSERT_EQUAL(2u, cs.itsSteps[1].id().nRanks());
+
+   // add flexible allocation, single rank per process
+   cs.addSubStep(ps, 1, CompositeStep::USE_ALL_AVAILABLE);
+   CPPUNIT_ASSERT_EQUAL(size_t(3u), cs.itsSteps.size());
+   CPPUNIT_ASSERT_EQUAL(7, cs.itsSteps[2].id().first());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[2].id().last());
+   CPPUNIT_ASSERT_EQUAL(1u, cs.itsSteps[2].id().nRanks());
+
+   // fixed allocation following a flexible allocation
+   cs.addSubStep(ps, 2, 1);
+   CPPUNIT_ASSERT_EQUAL(size_t(4u), cs.itsSteps.size());
+   CPPUNIT_ASSERT_EQUAL(-2, cs.itsSteps[3].id().first());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[3].id().last());
+   CPPUNIT_ASSERT_EQUAL(-3, cs.itsSteps[2].id().last());
+   CPPUNIT_ASSERT_EQUAL(7, cs.itsSteps[2].id().first());
+   CPPUNIT_ASSERT_EQUAL(2u, cs.itsSteps[3].id().nRanks());
+
+   // another fixed allocation following a flexible one,
+   // this time - single rank but two groups
+   cs.addSubStep(ps, 1, 2);
+   CPPUNIT_ASSERT_EQUAL(size_t(5u), cs.itsSteps.size());
+   CPPUNIT_ASSERT_EQUAL(-2, cs.itsSteps[4].id().first());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[4].id().last());
+   CPPUNIT_ASSERT_EQUAL(-4, cs.itsSteps[3].id().first());
+   CPPUNIT_ASSERT_EQUAL(-3, cs.itsSteps[3].id().last());
+   CPPUNIT_ASSERT_EQUAL(-5, cs.itsSteps[2].id().last());
+   CPPUNIT_ASSERT_EQUAL(7, cs.itsSteps[2].id().first());
+   CPPUNIT_ASSERT_EQUAL(2u, cs.itsSteps[3].id().nRanks());
+}
+
+void CompositeStepTest::testAddSubStepFlexMultiRank() {
+   CompositeStep cs;
+   // empty processing step to add to the composite
+   boost::shared_ptr<ProcessingStep> ps(new ProcessingStep);
+   CPPUNIT_ASSERT(ps);
+
+   // add flexible allocation, 3 ranks per process
+   // default number of groups mean the flexible allocation
+   cs.addSubStep(ps, 3);
+   CPPUNIT_ASSERT_EQUAL(size_t(1u), cs.itsSteps.size());
+   CPPUNIT_ASSERT_EQUAL(0, cs.itsSteps[0].id().first());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[0].id().last());
+   CPPUNIT_ASSERT_EQUAL(3u, cs.itsSteps[0].id().nRanks());
+
+   // fixed allocation following the flexible one
+   cs.addSubStep(ps, 1, 1);
+   CPPUNIT_ASSERT_EQUAL(size_t(2u), cs.itsSteps.size());
+   CPPUNIT_ASSERT_EQUAL(0, cs.itsSteps[0].id().first());
+   CPPUNIT_ASSERT_EQUAL(-2, cs.itsSteps[0].id().last());
+   CPPUNIT_ASSERT_EQUAL(3u, cs.itsSteps[0].id().nRanks());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[1].id().first());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[1].id().last());
+   CPPUNIT_ASSERT_EQUAL(1u, cs.itsSteps[1].id().nRanks());
+}
+
+
+void CompositeStepTest::testAddSubStepTwoFlex() {
+   CompositeStep cs;
+   // empty processing step to add to the composite
+   boost::shared_ptr<ProcessingStep> ps(new ProcessingStep);
+   CPPUNIT_ASSERT(ps);
+
+   // flexible
+   cs.addSubStep(ps, 1);
+   CPPUNIT_ASSERT_EQUAL(size_t(1u), cs.itsSteps.size());
+   CPPUNIT_ASSERT_EQUAL(0, cs.itsSteps[0].id().first());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[0].id().last());
+   CPPUNIT_ASSERT_EQUAL(1u, cs.itsSteps[0].id().nRanks());
+   // fixed
+   cs.addSubStep(ps, 1,1);
+   CPPUNIT_ASSERT_EQUAL(size_t(2u), cs.itsSteps.size());
+   CPPUNIT_ASSERT_EQUAL(0, cs.itsSteps[0].id().first());
+   CPPUNIT_ASSERT_EQUAL(-2, cs.itsSteps[0].id().last());
+   CPPUNIT_ASSERT_EQUAL(1u, cs.itsSteps[0].id().nRanks());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[1].id().first());
+   CPPUNIT_ASSERT_EQUAL(-1, cs.itsSteps[1].id().last());
+   CPPUNIT_ASSERT_EQUAL(1u, cs.itsSteps[1].id().nRanks());
+
+   // attempting to have another flexible allocation - this should
+   // throw an exception
+   cs.addSubStep(ps, 1);
 }
 
 } // end of namespace askapparallel
