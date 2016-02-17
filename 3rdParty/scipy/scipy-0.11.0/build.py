89c6edaaa3497ca0d74b8f467221e9d6fa2ae2b2
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2016-02-17 14:49:38 +1100

    added the Darwin tests and extra flags for the compilation of scipy on Yosemite+. All these changes should be hidden from other architectures and even OSX versions

--- 3rdParty/scipy/scipy-0.11.0/build.py
+++ 3rdParty/scipy/scipy-0.11.0/build.py
@@ -1,6 +1,7 @@
 import os.path
 
 from askapdev.rbuild.builders import Setuptools as Builder
+import askapdev.rbuild.utils as utils
 
 builder = Builder()
 builder.remote_archive = "scipy-0.11.0.tar.gz"
@@ -12,6 +13,9 @@ lapacklib = os.path.join(lapack, 'lib/liblapack.a')
 
 builder.add_env("BLAS", blaslib)
 builder.add_env("LAPACK", lapacklib)
+platform = utils.get_platform()
+if platform['system'] == 'Darwin':
+    if (int(platform['tversion'][1]) >= 10):
+        builder.add_env("FFLAGS",'-Wa,-q')
 builder.nowarnings = True
-
 builder.build()
