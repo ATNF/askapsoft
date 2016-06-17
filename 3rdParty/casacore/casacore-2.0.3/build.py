19b64b981508de2df2389a8201d7881bec3b3ac8
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2016-06-17 16:21:47 +1000

    fixes to casacore build.py

--- 3rdParty/casacore/casacore-2.0.3/build.py
+++ 3rdParty/casacore/casacore-2.0.3/build.py
@@ -12,13 +12,8 @@ builder.remote_archive = "casacore-2.0.3.tar.gz"
 
 cfitsio = builder.dep.get_install_path("cfitsio")
 wcslib  = builder.dep.get_install_path("wcslib")
-blas    = builder.dep.get_install_path("blas")
-lapack  = builder.dep.get_install_path("lapack")
 fftw3   = builder.dep.get_install_path("fftw3")
 
-# CMake doesn't know about ROOT_DIR for these packages, so be explicit
-builder.add_option("-DBLAS_LIBRARIES=%s" % os.path.join(blas, 'lib', 'libblas.a'))
-builder.add_option("-DLAPACK_LIBRARIES=%s" % os.path.join(lapack, 'lib', 'liblapack.a'))
 # these work
 builder.add_option("-DCFITSIO_ROOT_DIR=%s" % cfitsio)
 builder.add_option("-DWCSLIB_ROOT_DIR=%s" % wcslib)
@@ -39,6 +34,13 @@ if platform['system'] == 'Darwin':
     if (int(platform['tversion'][1]) >= 10):
         builder.add_option("-DCMAKE_Fortran_FLAGS=-Wa,-q")
 else:
+# On darwin casa can spot the accelerate framework    
+    blas    = builder.dep.get_install_path("blas")
+    lapack  = builder.dep.get_install_path("lapack")
+    
+    # CMake doesn't know about ROOT_DIR for these packages, so be explicit
+    builder.add_option("-DBLAS_LIBRARIES=%s" % os.path.join(blas, 'lib', 'libblas.a'))
+    builder.add_option("-DLAPACK_LIBRARIES=%s" % os.path.join(lapack, 'lib', 'liblapack.a'))
     builder.add_option("-DCMAKE_C_COMPILER=gcc")
     builder.add_option("-DCMAKE_CXX_COMPILER=g++")
 
