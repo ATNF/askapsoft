6bed4ab7750db039e784e74bfe11e40cc0906491
DanielCollins <DanielCollins@csiro.au>
2016-07-04 07:02:23 +0000

    ASKAPSDP-2050: this appears to be a minimal working test case

--- Code/Components/Services/manager/current/functests/test_service.py
+++ Code/Components/Services/manager/current/functests/test_service.py
@@ -1,18 +1,18 @@
 """Test CP Manager service interface
 """
+
 from common import *
-from nose.tools import assert_equals
-import time
+
+import nose
+from unittest import skip
 
 class TestCPManagerService(object):
     def __init__(self):
         self.igsession = None
         self.service = None
-        self.pointname1 = "site.test.point1"
-        self.pmap = {}
 
     def setUp(self):
-        os.environ["ICE_CONFIG"] = "ice.cfg"
+        os.environ["ICE_CONFIG"] = "config-files/ice.cfg"
         self.igsession = IceSession("applications.txt", cleanup=True)
         try:
             self.igsession.start()
@@ -30,6 +30,11 @@ class TestCPManagerService(object):
         self.igsession = None
 
     def test_get_service_version(self):
-        version = self.service.getServiceVersion()
-        print version
-        assert len(version) > 0
+        #Don't test the full string, as the version changes with SVN revision or tag.
+        process_name = self.service.getServiceVersion().split(';')[0]
+        assert 'manager' == process_name
+
+    def test_start_abort_wait_observation_sequence(self):
+        self.service.startObs(0)
+        self.service.abortObs()
+        self.service.waitObs(-1)
