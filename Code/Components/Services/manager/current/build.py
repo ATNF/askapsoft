1fdda40db552ed5d0f6c0674d1ab99ea21ff0e90
DanielCollins <DanielCollins@csiro.au>
2016-07-07 08:05:07 +0000

    ASKAPSDP-2080: resolved with monkey patch solution

--- Code/Components/Services/manager/current/build.py
+++ Code/Components/Services/manager/current/build.py
@@ -1,3 +1,4 @@
+import sys
 from askapdev.rbuild.builders import Ant as Builder
 from askapdev.rbuild.builders import Data as DataBuilder
 
@@ -8,5 +9,10 @@ builder.build()
 
 # deployment-specific files, i.e. start scripts
 databuilder = DataBuilder("scripts")
+# Monkey patch the data builder so it doesn't try to run any tests.
+# It doesn't implement any, and it causes errors when running functional tests.
+# See https://jira.csiro.au/browse/ASKAPSDP-2080
+databuilder._functest = lambda: None
+databuilder._test = lambda: None
 databuilder.do_clean = False
 databuilder.build()
