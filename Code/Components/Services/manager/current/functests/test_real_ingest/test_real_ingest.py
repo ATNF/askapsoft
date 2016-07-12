5bf9a781a22b0f4aa2f63a6718ad19e8f8f361c8
DanielCollins <DanielCollins@csiro.au>
2016-07-12 04:13:44 +0000

    ASKAPSDP-1999: fixed import errors in Python functional tests. Refactored test code, doing away with common files. The imports are easier to manage when each test is self contained.

--- Code/Components/Services/manager/current/functests/test_real_ingest/test_real_ingest.py
+++ Code/Components/Services/manager/current/functests/test_real_ingest/test_real_ingest.py
@@ -1,12 +1,16 @@
 """Test CP Manager service interface
 """
 
-from common import *
-
-import nose
+import os
 from unittest import skip
 
-class TestMockIngest(object):
+from askap.iceutils import IceSession, get_service_object
+
+# always import from askap.slice before trying to import any interfaces
+from askap.slice import CP
+from askap.interfaces.cp import ICPObsServicePrx
+
+class TestRealIngest(object):
     def __init__(self):
         self.igsession = None
         self.service = None
@@ -15,7 +19,7 @@ class TestMockIngest(object):
         # Note that the working directory is 'functests', thus paths are
         # relative to that location.
         os.environ["ICE_CONFIG"] = "config-files/ice.cfg"
-        os.environ['TEST_DIR'] = 'test_mock_ingest'
+        os.environ['TEST_DIR'] = 'test_real_ingest'
         self.igsession = IceSession(
             os.path.expandvars("$TEST_DIR/applications.txt"),
             cleanup=True)
@@ -24,7 +28,7 @@ class TestMockIngest(object):
             self.service = get_service_object(
                 self.igsession.communicator,
                 "CentralProcessorService@CentralProcessorAdapter",
-                askap.interfaces.cp.ICPObsServicePrx
+                ICPObsServicePrx
             )
         except Exception, ex:
             self.igsession.terminate()
@@ -34,12 +38,28 @@ class TestMockIngest(object):
         self.igsession.terminate()
         self.igsession = None
 
+    # @skip('too slow!')
     def test_get_service_version(self):
         #Don't test the full string, as the version changes with SVN revision or tag.
+        print "Calling getServiceVersion...",
         process_name = self.service.getServiceVersion().split(';')[0]
+        print "DONE"
         assert 'manager' == process_name
 
+    # @skip('too slow!')
     def test_start_abort_wait_observation_sequence(self):
+        print "Starting observation...",
         self.service.startObs(0)
+        print "DONE"
+
+        print "Waiting 5s for observation to complete (it shouldn't)...",
+        self.service.waitObs(5000)
+        print "DONE"
+
+        print "Aborting observation...",
         self.service.abortObs()
+        print "DONE"
+
+        print "Waiting for observation to abort...",
         self.service.waitObs(-1)
+        print "DONE"
--- Code/Components/Services/manager/current/functests/test_sb_state_change/test_sb_state_change.py
+++ Code/Components/Services/manager/current/functests/test_sb_state_change/test_sb_state_change.py
@@ -1,33 +1,38 @@
 """Test CP Manager schedulingblock state transition handling
 """
 
-import sys
-import os
-import time
 import datetime
-import threading
 import nose
+import os
+import sys
+import time
+import threading
 
 from nose.tools import *
 from unittest import skip
 
-import askap
-import askap.interfaces.schedblock
-import Ice
-import IceStorm
-
 from askap.iceutils import IceSession, get_service_object
+
+# always import from askap.slice before trying to import any interfaces
 from askap.slice import (
     CommonTypes,
     CP,
     QueueService,
     ObsProgramService,
     SchedulingBlockService,
-    DataServiceExceptions)
-from askap.ds.models.schedulingblock import sb_states as SB_STATES
+    DataServiceExceptions,
+)
+
+import askap
+import Ice
+import IceStorm
+from askap.interfaces import (
+    schedblock,
+)
 
 
 class TestSbStateChange(object):
 
     def test_stub(self):
+        print(dir(schedblock.ObsState))
         pass
