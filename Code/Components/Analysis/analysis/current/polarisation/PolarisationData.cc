333c3597fd51488934eba8ed4afdac22dfb501ac
MatthewWhiting <MatthewWhiting@csiro.au>
2016-11-27 11:03:33 +0000

    ASKAPSDP-1196 - Adding missing files Had left out the PolarisationData files from rev.8118.

---
+++ Code/Components/Analysis/analysis/current/polarisation/PolarisationData.cc
@@ -0,0 +1,119 @@
+/// @file
+///
+/// Extracting spectra and other polarised data from continuum cubes,
+/// ready for Rotation Measure Synthesis.
+///
+/// @copyright (c) 2016 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Matthew Whiting <Matthew.Whiting@csiro.au>
+///
+#include <polarisation/PolarisationData.h>
+#include <askap_analysis.h>
+
+#include <askap/AskapLogging.h>
+#include <askap/AskapError.h>
+
+#include <extraction/SourceSpectrumExtractor.h>
+#include <extraction/NoiseSpectrumExtractor.h>
+
+#include <casacore/casa/Quanta/Quantum.h>
+#include <Common/ParameterSet.h>
+
+///@brief Where the log messages go.
+ASKAP_LOGGER(logger, ".poldata");
+
+namespace askap {
+
+namespace analysis {
+
+PolarisationData::PolarisationData(const LOFAR::ParameterSet &parset):
+    itsParset(parset),
+    itsStokesI(parset,"I"),
+    itsStokesQ(parset,"Q"),
+    itsStokesU(parset,"U")
+{
+    // store the locations of the images
+
+    // record extraction parameters
+    
+    itsParset.replace("useDetectedPixels", "false");
+    itsParset.replace("scaleSpectraByBeam", "true");
+    
+    
+}
+
+void PolarisationData::initialise(CasdaComponent *comp)
+{
+
+    // extract Stokes I,Q,U and noise spectra
+    itsStokesI.setComponent(comp);
+    itsStokesI.extract();
+    unsigned int size=itsStokesI.spectrum().size();
+    itsStokesQ.setComponent(comp);
+    itsStokesQ.extract();
+    itsStokesU.setComponent(comp);
+    itsStokesU.extract();
+
+    // write out extracted spectra
+    if (itsParset.getBool("writeSpectra","true")) {
+        itsStokesI.write();
+        itsStokesQ.write();
+        itsStokesU.write();
+    }
+
+    // compute "average noise"
+    itsAverageNoiseSpectrum = (itsStokesQ.noiseSpectrum() +
+                               itsStokesU.noiseSpectrum() ) / 2.;    
+     
+    // get frequency array and compute lambda-squared array
+    itsFrequencies = itsStokesI.frequencies();
+    ASKAPASSERT(itsFrequencies.size()==size);
+    itsLambdaSquared = casa::Vector<Float>(size);
+    for(unsigned int i=0;i<size;i++){
+        float lambda = QC::c.getValue() / itsFrequencies[i];
+        itsLambdaSquared[i] = lambda*lambda;
+    }
+
+    // compute model I spectrum - use alpha/beta from component
+    float flux0 = casa::Quantum<float>(comp->intFlux(),
+                                       casda::intFluxUnitContinuum).getValue(
+                                           itsStokesI.bunit());
+    float nu0 = casa::Quantum<float>(comp->freq(),casda::freqUnit).getValue(itsStokesI.freqUnit());
+                                     
+    ASKAPLOG_DEBUG_STR(logger, "PolarisationData's flux0="<<flux0 << " at freq="<<nu0);
+    ASKAPLOG_DEBUG_STR(logger, itsFrequencies);
+    float alpha = comp->alpha();
+    float beta = comp->beta();
+
+    itsModelStokesI = casa::Vector<Float>(size);
+    for(unsigned int i=0;i<size;i++){
+        float lognu = log(itsFrequencies[i]/nu0);
+        float logflux = log(flux0) + alpha*lognu + beta*lognu*lognu;
+        itsModelStokesI[i] = exp(logflux);
+    }
+    
+}
+
+
+}
+}
---
+++ Code/Components/Analysis/analysis/current/polarisation/PolarisationData.h
@@ -0,0 +1,110 @@
+/// @file
+///
+/// Class to hold the input data for the polarisation pipeline
+///
+/// @copyright (c) 2016 CSIRO
+/// Australia Telescope National Facility (ATNF)
+/// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+/// PO Box 76, Epping NSW 1710, Australia
+/// atnf-enquiries@csiro.au
+///
+/// This file is part of the ASKAP software distribution.
+///
+/// The ASKAP software distribution is free software: you can redistribute it
+/// and/or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 2 of the License,
+/// or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+///
+/// @author Matthew Whiting <Matthew.Whiting@csiro.au>
+///
+#ifndef ASKAP_ANALYSIS_POL_DATA_H_
+#define ASKAP_ANALYSIS_POL_DATA_H_
+
+#include <polarisation/StokesSpectrum.h>
+#include <catalogues/CasdaComponent.h>
+
+#include <casacore/casa/Arrays/Array.h>
+
+#include <Common/ParameterSet.h>
+
+namespace askap {
+
+namespace analysis {
+
+class PolarisationData {
+    public:
+        PolarisationData(const LOFAR::ParameterSet &parset);
+        virtual ~PolarisationData(){};
+
+        void initialise(CasdaComponent *comp);
+
+        StokesSpectrum &I()
+        {
+            StokesSpectrum &ref = itsStokesI; return ref;
+        }
+        StokesSpectrum &Q()
+        {
+            StokesSpectrum &ref = itsStokesQ; return ref;
+        }
+        StokesSpectrum &U()
+        {
+            StokesSpectrum &ref = itsStokesU; return ref;
+        }
+
+        casa::Vector<float> &Imod()
+        {
+            casa::Vector<float> &ref = itsModelStokesI; return ref;
+        }
+        casa::Vector<float> &noise()
+        {
+            casa::Vector<float> &ref = itsAverageNoiseSpectrum; return ref;
+        }
+        casa::Vector<float> &l2()
+        {
+            casa::Vector<float> &ref = itsLambdaSquared; return ref;
+        }
+
+    protected:
+
+        LOFAR::ParameterSet itsParset;
+    
+        /// @brief Spectra extracted from cubes
+        /// {
+        StokesSpectrum itsStokesI;
+        StokesSpectrum itsStokesQ;
+        StokesSpectrum itsStokesU;
+        /// }
+
+        casa::Vector<float> itsModelStokesI;
+
+        casa::Vector<float> itsAverageNoiseSpectrum;
+
+        casa::Vector<float> itsFrequencies;
+        casa::Vector<float> itsLambdaSquared;
+        casa::Vector<float> itsStokesIcoeffs;
+
+        /// @brief Width of extraction box, defined through parset
+        unsigned int itsExtractBoxSize;
+
+        float itsBoxNormalisation;
+
+};
+
+
+}
+
+}
+
+
+
+
+#endif
