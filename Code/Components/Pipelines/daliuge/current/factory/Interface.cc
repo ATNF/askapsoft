d7ff41014964428461a675722b28cc5e476673a8
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2017-06-26 16:32:48 +1000

    added string.h to the Interface.cc as the CI build was failing

--- Code/Components/Pipelines/daliuge/current/factory/Interface.cc
+++ Code/Components/Pipelines/daliuge/current/factory/Interface.cc
@@ -8,44 +8,46 @@
 #include<factory/DaliugeApplicationFactory.h>
 #include<factory/Interface.h>
 
+#include<string.h>
 
-    int init(dlg_app_info *app, const char ***arguments) {
-        // this means we have to instantiate an application
-        // and call its init
-        const char **param = arguments[0];
-        bool got_name = false;
-        while (1) {
 
-            // Sentinel
-            if (*param == NULL) {
-                break;
-            }
+int init(dlg_app_info *app, const char ***arguments) {
+    // this means we have to instantiate an application
+    // and call its init
+    const char **param = arguments[0];
+    bool got_name = false;
+    while (1) {
 
-            if (strcmp(param[0], "name") == 0) {
-                app->appname = strdup(param[1]);
-                got_name = true;
-            }
-
-            param++;
+        // Sentinel
+        if (*param == NULL) {
+            break;
         }
-        if (got_name == false) {
-            app->appname = strdup("Example");
+
+        if (strcmp(param[0], "name") == 0) {
+            app->appname = strdup(param[1]);
+            got_name = true;
         }
-        // need to set the app->appname here .... from the arguments ....
-        askap::DaliugeApplication::ShPtr thisApp = askap::DaliugeApplicationFactory::make(app->appname);
-        return thisApp->init(app, arguments);
+
+        param++;
     }
-    int run(dlg_app_info *app) {
-        askap::DaliugeApplication::ShPtr thisApp = askap::DaliugeApplicationFactory::make(app->appname);
-        return thisApp->run(app);
+    if (got_name == false) {
+        app->appname = strdup("Example");
     }
-    void data_written(dlg_app_info *app, const char *uid,
-        const char *data, size_t n) {
-            askap::DaliugeApplication::ShPtr thisApp = askap::DaliugeApplicationFactory::make(app->appname);
-            thisApp->data_written(app, uid, data, n);
+    // need to set the app->appname here .... from the arguments ....
+    askap::DaliugeApplication::ShPtr thisApp = askap::DaliugeApplicationFactory::make(app->appname);
+    return thisApp->init(app, arguments);
+}
+int run(dlg_app_info *app) {
+    askap::DaliugeApplication::ShPtr thisApp = askap::DaliugeApplicationFactory::make(app->appname);
+    return thisApp->run(app);
+}
+void data_written(dlg_app_info *app, const char *uid,
+    const char *data, size_t n) {
+        askap::DaliugeApplication::ShPtr thisApp = askap::DaliugeApplicationFactory::make(app->appname);
+        thisApp->data_written(app, uid, data, n);
     }
     void drop_completed(dlg_app_info *app, const char *uid,
         drop_status status) {
             askap::DaliugeApplication::ShPtr thisApp = askap::DaliugeApplicationFactory::make(app->appname);
             thisApp->drop_completed(app, uid, status);
-    }
+        }
