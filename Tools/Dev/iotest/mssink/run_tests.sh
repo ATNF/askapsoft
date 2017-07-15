1b543f57aa3b92ac08b6aa8099de86c4fe231d9a
StephenOrd <StephenOrd@csiro.au>
2017-07-15 15:03:31 +0000

    changed the behviour of the tmssink cripts in iotest to allow more efficient running of batches of configurations

--- Tools/Dev/iotest/mssink/run_tests.sh
+++ Tools/Dev/iotest/mssink/run_tests.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -l
+#workdirectory - should make this an argument
+module load askapsoft
+
+workdir=/astro/askap/sord
 
 #number of antennas
 
 antennas=(12 24 36)
-channels=(10368 16200)
+channels=(10368 17280)
 ranks=(1 2 4 8 40)
 invocations=(1 36)
 numberofnodes=8
 minutes=1
-tester="run_direct.sh "
+tester="${ASKAP_ROOT}/Tools/Dev/iotest/mssink/run.sh "
+
 # these are the tests
 # (invocations ranks antennas channels minutes)
 
@@ -46,28 +51,55 @@ TESTARRAY=(
     "${ES2A[*]}"
     "${ES2B[*]}"
     "${ES2C[*]}"
-    "${ES3[*]}"
+#    "${ES3[*]}"
     "${ES4[*]}"
     "${ES5A[*]}"
     "${ES5B[*]}"
     "${ES5C[*]}"
-    "${ES6[*]}"
+#    "${ES6[*]}"
     "${FA1[*]}"
     "${FA2A[*]}"
     "${FA2B[*]}"
     "${FA2C[*]}"
-    "${FA3[*]}"
+#    "${FA3[*]}"
 )
     
 #Split by frequency
 cnt=${#TESTARRAY[@]}
 
 echo "Number of tests: $cnt"
+cd $workdir
 
 for (( i = 0 ; i < cnt ; i++ ))
 do
     test=${TESTARRAY[$i]}   
-    line=`echo $test | awk '{print ""$1" -n "$3" -a "$4" -C "$5" -t "$6" -d "$7" \n"}'`
+    line=`echo $test | awk '{print ""$1" -n "$3" -a "$4" -C "$5" -t "$6" -d "$7" -T 1\n"}'`
     echo $line
-
+    eval $line
+    
 done
+
+for (( i = 0 ; i < cnt ; i++ ))
+do
+    test=${TESTARRAY[$i]}   
+    t=`echo $test | awk '{print ""$7""}'`
+    if [[ ${i} -lt 1 ]]; then
+        sbatch="tMSSink_${t}.sbatch"
+        echo "submitting $sbatch"
+        jid1=`sbatch ${sbatch} | cut -d " " -f 4`
+        post_batch_file="post_${t}.sbatch"
+        cmd="sbatch --dependency=afterany:${jid1} ${post_batch_file}"
+        echo $cmd
+        eval $cmd
+    else
+        sbatch="tMSSink_${t}.sbatch"
+        echo "submitting $sbatch"
+        jid1=`sbatch --dependency=afterany:${jid1} ${sbatch} | cut -d " " -f 4`
+        post_batch_file="post_${t}.sbatch"
+        cmd="sbatch --dependency=afterany:${jid1} ${post_batch_file}"
+        echo $cmd
+        eval $cmd
+
+    fi
+done
+
