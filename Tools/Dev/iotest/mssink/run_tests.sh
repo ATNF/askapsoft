1e4b6cd917cb90d129eddc4dc56d45354b91e940
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2017-07-17 10:54:41 +1000

    added support for multiple tasks per node

--- Tools/Dev/iotest/mssink/run_tests.sh
+++ Tools/Dev/iotest/mssink/run_tests.sh
@@ -9,6 +9,7 @@ workdir=/astro/askap/sord
 antennas=(12 24 36)
 channels=(10368 17280)
 ranks=(1 2 4 8 40)
+ntasks_per_node=(1 4 8)
 invocations=(1 36)
 numberofnodes=8
 minutes=1
@@ -18,28 +19,28 @@ tester="${ASKAP_ROOT}/Tools/Dev/iotest/mssink/run.sh "
 # (invocations ranks antennas channels minutes)
 
 # Early science split by frequency
-ES1=($tester ${invocations[0]} ${ranks[0]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES1" )
-ES2A=($tester ${invocations[0]} ${ranks[1]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES2A" )
-ES2B=($tester ${invocations[0]} ${ranks[2]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES2B") 
-ES2C=($tester ${invocations[0]} ${ranks[3]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES2C") 
+ES1=($tester ${invocations[0]} ${ranks[0]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES1" ${ntasks_per_node[0]})
+ES2A=($tester ${invocations[0]} ${ranks[1]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES2A" ${ntasks_per_node[0]})
+ES2B=($tester ${invocations[0]} ${ranks[2]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES2B" ${ntasks_per_node[0]}) 
+ES2C=($tester ${invocations[0]} ${ranks[3]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES2C" ${ntasks_per_node[0]}) 
 
 # Early science split by beam
-ES3=($tester ${invocations[1]} ${ranks[0]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES3")
+ES3=($tester ${invocations[1]} ${ranks[0]} ${antennas[0]} ${channels[0]} ${minutes[0]} "ES3" )
 
 # Early science ++ split by frequency
-ES4=($tester ${invocations[0]} ${ranks[0]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES4")
-ES5A=($tester ${invocations[0]} ${ranks[1]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES5A")
-ES5B=($tester ${invocations[0]} ${ranks[2]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES5B") 
-ES5C=($tester ${invocations[0]} ${ranks[3]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES5C")
+ES4=($tester ${invocations[0]} ${ranks[0]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES4") ${ntasks_per_node[0]}
+ES5A=($tester ${invocations[0]} ${ranks[1]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES5A" ${ntasks_per_node[0]})
+ES5B=($tester ${invocations[0]} ${ranks[2]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES5B" ${ntasks_per_node[0]}) 
+ES5C=($tester ${invocations[0]} ${ranks[3]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES5C" ${ntasks_per_node[0]})
 
 # Early science ++ split by beam
 ES6=($tester ${invocations[1]} ${ranks[0]} ${antennas[1]} ${channels[0]} ${minutes[0]} "ES6")
 
 # Full ASKAP split by frequency
-FA1=($tester ${invocations[0]} ${ranks[0]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA1")
-FA2A=($tester ${invocations[0]} ${ranks[2]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA2A")
-FA2B=($tester ${invocations[0]} ${ranks[3]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA2B")
-FA2C=($tester ${invocations[0]} ${ranks[4]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA2C")
+FA1=($tester ${invocations[0]} ${ranks[0]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA1" ${ntasks_per_node[0]})
+FA2A=($tester ${invocations[0]} ${ranks[2]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA2A" ${ntasks_per_node[0]})
+FA2B=($tester ${invocations[0]} ${ranks[3]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA2B" ${ntasks_per_node[0]})
+FA2C=($tester ${invocations[0]} ${ranks[4]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA2C" ${ntasks_per_node[2]})
 
 # Full ASKAP split by beam
 FA3=($tester ${invocations[1]} ${ranks[0]} ${antennas[2]} ${channels[1]} ${minutes[0]} "FA3")
@@ -73,12 +74,12 @@ cd $workdir
 for (( i = 0 ; i < cnt ; i++ ))
 do
     test=${TESTARRAY[$i]}   
-    line=`echo $test | awk '{print ""$1" -n "$3" -a "$4" -C "$5" -t "$6" -d "$7" -T 1\n"}'`
+    line=`echo $test | awk '{print ""$1" -n "$3" -a "$4" -C "$5" -t "$6" -d "$7" -T 1 -w "$8" \n"}'`
     echo $line
     eval $line
     
 done
-
+# submit with dependencies so not everyone tries to hit the disk at once
 for (( i = 0 ; i < cnt ; i++ ))
 do
     test=${TESTARRAY[$i]}   
