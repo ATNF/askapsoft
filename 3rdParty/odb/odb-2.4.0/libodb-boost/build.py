4c9e93609d9b4eec6bebe49d473f57579f37d2c4
DanielCollins <DanielCollins@csiro.au>
2017-09-12 03:56:48 +0000

    fixing linking issue with boost that was causing a mismatch between system and 3rdparty boost versions

--- 3rdParty/odb/odb-2.4.0/libodb-boost/build.py
+++ 3rdParty/odb/odb-2.4.0/libodb-boost/build.py
@@ -7,11 +7,15 @@ builder = Builder(pkgname="libodb-boost-2.4.0")
 # setup the libodb dependency
 libodb = builder.dep.get_install_path("libodb")
 builder.add_option("CPPFLAGS=-I{0}/include".format(libodb))
-builder.add_option("LDFLAGS=-L{0}/lib".format(libodb))
 
 # setup the boost dependency
 boost = builder.dep.get_install_path("boost")
 builder.add_option("--with-boost={0}/include".format(boost))
+
+# Set LDFLAGS
+builder.add_option("LDFLAGS='-L{0}/lib -L{1}/lib'".format(
+    libodb,
+    boost))
 
 builder.remote_archive = "libodb-boost-2.4.0.tar.gz"
 builder.nowarnings = True
