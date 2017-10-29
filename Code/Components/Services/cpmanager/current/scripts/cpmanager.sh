a1894b2a9fa759c446a266617a017afe03888bdf
Wu, Xinyu (CASS, Marsfield) <Xinyu.Wu@csiro.au>
2017-10-29 19:42:55 +1100

    put icehost back

--- Code/Components/Services/cpmanager/current/scripts/cpmanager.sh
+++ Code/Components/Services/cpmanager/current/scripts/cpmanager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# export ICE_CONFIG="../files/config/ingestmanager.ice_cfg,../files/config/icehost.ice_cfg"
-export ICE_CONFIG="../files/config/ingestmanager.ice_cfg"
+export ICE_CONFIG="../files/config/ingestmanager.ice_cfg,../files/config/icehost.ice_cfg"
+# export ICE_CONFIG="../files/config/ingestmanager.ice_cfg"
 export PYTHON_LOG_FILE=./cpmanager.log
 
 ./ingestservice.py --log-config=../files/config/ingestmanager.pylog_cfg
