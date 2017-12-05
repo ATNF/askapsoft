d0299d4d53ca05e0d1c6815416bd21152e94e898
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2017-12-05 17:01:05 +1100

    ASKAPSDP-2889 added a temporary fix to put python dependencies into the virtual env - this should fix the CI build until we work out what to do

--- Tools/virtualenv/bootstrap.py
+++ Tools/virtualenv/bootstrap.py
@@ -2,14 +2,22 @@ import shutil
 import os
 import sys
 import urllib2
+import ConfigParser
 
-version = "1.11.4"
+#version = "1.11.4"
+#version = "12.0"
+version = "14.0.6"
 #version = "14.0.6"
 virtualenv = "virtualenv-%s" % version
 python_ver = sys.version[:3]
 python_exe = sys.executable
 askap_root = os.environ.get("ASKAP_ROOT", os.path.abspath("../../"))
 install_dir = "%s/install" % os.getcwd()
+
+cfg_parser = ConfigParser.ConfigParser()
+config_file = "%s/bootstrap.ini" % askap_root
+cfg_parser.read(config_file)
+
 
 if os.path.isdir(install_dir):
     shutil.rmtree(install_dir)
@@ -32,3 +40,7 @@ os.chdir("%s" % virtualenv)
 os.system("%s virtualenv.py --no-site-packages %s" % (python_exe, askap_root))
 # For use with release process.
 os.system("%s virtualenv.py --no-site-packages %s" % (python_exe, install_dir))
+
+for name, version in cfg_parser.items("Python Dependencies"):
+        install_line="source %s/bin/activate && pip install %s" % (askap_root,version)
+        os.system(install_line)
