7c6cd436461eea7812eca7a090965cc7da78e24a
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2017-12-06 10:53:01 +1100

    wheezy uses dash which does not support source

--- Tools/virtualenv/bootstrap.py
+++ Tools/virtualenv/bootstrap.py
@@ -42,7 +42,7 @@ os.system("%s virtualenv.py --no-site-packages %s" % (python_exe, askap_root))
 os.system("%s virtualenv.py --no-site-packages %s" % (python_exe, install_dir))
 
 for name, version in cfg_parser.items("Python Dependencies"):
-        install_line="source %s/bin/activate && pip install %s" % (askap_root,version)
+        install_line=". %s/bin/activate && pip install %s" % (askap_root,version)
         
         print "running ", install_line
         os.system(install_line)
