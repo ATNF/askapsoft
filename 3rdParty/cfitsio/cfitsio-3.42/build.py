2c81e7331f27c3821f96a8fd3acf43dbf4c62da2
DanielMitchell <DanielMitchell@csiro.au>
2018-01-12 05:19:17 +0000

    uncommented builder.nowarnings=True in cfitsio-3.42/build.py

--- 3rdParty/cfitsio/cfitsio-3.42/build.py
+++ 3rdParty/cfitsio/cfitsio-3.42/build.py
@@ -4,7 +4,7 @@ from askapdev.rbuild.builders import Autotools as Builder
 
 builder = Builder("cfitsio", buildtargets=['shared'])
 builder.remote_archive = "cfitsio3420.tar.gz"
-#builder.nowarnings = True
+builder.nowarnings = True
 
 if os.path.basename(os.getenv('FC')) == "pgfortran":
     builder.add_env("CPPFLAGS", "-DpgiFortran")
