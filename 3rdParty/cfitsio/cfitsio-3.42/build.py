872c1de2364f7e9a11db41c022b1174788787ab1
TonyMaher <TonyMaher@csiro.au>
2018-01-12 03:47:15 +0000

    ASKAPSDP-2811 In cfitsio build.py, use the basename of os.getenv('FC') to compare with "pgfortran".

--- 3rdParty/cfitsio/cfitsio-3.42/build.py
+++ 3rdParty/cfitsio/cfitsio-3.42/build.py
@@ -6,7 +6,7 @@ builder = Builder("cfitsio", buildtargets=['shared'])
 builder.remote_archive = "cfitsio3420.tar.gz"
 #builder.nowarnings = True
 
-if os.getenv('FC') == "pgfortran":
-    builder.add_env("CFLAGS", "-DpgiFortran")
+if os.path.basename(os.getenv('FC')) == "pgfortran":
+    builder.add_env("CPPFLAGS", "-DpgiFortran")
 
 builder.build()
