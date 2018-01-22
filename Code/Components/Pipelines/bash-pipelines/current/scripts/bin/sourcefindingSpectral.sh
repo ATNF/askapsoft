c5096ec81c42090bbd72ae79fc78376a2f2d464c
MatthewWhiting <MatthewWhiting@csiro.au>
2018-01-22 12:29:09 +0000

    ASKAPSDP-2916 - Further fits extension in the spectral catalogue names

--- Code/Components/Pipelines/bash-pipelines/current/scripts/bin/sourcefindingSpectral.sh
+++ Code/Components/Pipelines/bash-pipelines/current/scripts/bin/sourcefindingSpectral.sh
@@ -189,7 +189,7 @@ Selavy.nsubx = ${SELAVY_SPEC_NSUBX}
 Selavy.nsuby = ${SELAVY_SPEC_NSUBY}
 Selavy.nsubz = ${SELAVY_SPEC_NSUBZ}
 #
-Selavy.resultsFile = selavy-${imageName}.txt
+Selavy.resultsFile = selavy-\${fitsimage%%.fits}.txt
 #
 \${weightpars}
 #
