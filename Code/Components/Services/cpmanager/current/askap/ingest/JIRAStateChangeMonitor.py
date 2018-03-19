edd91af79fe8a288bcd398d25fafa1891a6e8a3a
XinyuWu <XinyuWu@csiro.au>
2018-03-19 03:49:04 +0000

    ASKAPSDP-2849: cpmanager deployment Fixed the following problem: -! 02/07/18 09:11:37.553 warning: dispatch exception: Util.cpp:333: Ice::UnknownException:    unknown exception:    exceptions.TypeError: changed() takes exactly 4 arguments (5 given)

--- Code/Components/Services/cpmanager/current/askap/ingest/JIRAStateChangeMonitor.py
+++ Code/Components/Services/cpmanager/current/askap/ingest/JIRAStateChangeMonitor.py
@@ -6,13 +6,14 @@ import subprocess
 import time
 import os
 from askap.jira import Jira
-
+from . import logger
 
 class JIRAStateChangeMonitor(ISBStateMonitor):
     def __init__(self, params):
         self.params = params
         self.jira = Jira()
 
-    def changed(self, sbid, new_state, update_time):
+    def changed(self, sbid, new_state, update_time, current=None):
         if new_state.name == ObsState.OBSERVERD:
+            logger.info("Raise jira for " + str(sbid) + ": Ready for data processing")
             self.jira.create_issue(sbid, "Ready for data processing")
