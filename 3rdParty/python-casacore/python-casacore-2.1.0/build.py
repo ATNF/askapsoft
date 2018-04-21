fb0880523263bf9b02af638f4cf76203b456c5bb
MatthewWhiting <MatthewWhiting@csiro.au>
2018-04-21 11:38:03 +0000

    ASKAPSDP-2997 - Adding python-casacore to 3rdParty
    This provides the python-casacore library, with a build script that 
    enables it to be built completely.

---
+++ 3rdParty/python-casacore/python-casacore-2.1.0/build.py
@@ -0,0 +1,18 @@
+from askapdev.rbuild.builders import Setuptools as Builder
+
+builder = Builder()
+builder.remote_archive = "python-casacore-2.1.0.tar.gz"
+
+# Need this to ensure _build() runs with the "python setup.py build_ext" command
+builder.has_extension = True
+
+wcslib = builder.dep.get_install_path("wcslib")
+cfitsio = builder.dep.get_install_path("cfitsio")
+boost   = builder.dep.get_install_path("boost")
+numpy   = builder.dep.get_install_path("numpy")
+casacore = builder.dep.get_install_path("casacore")
+
+builder.add_option("-I%s/include:%s/include:%s/include:%s/include"%(casacore,boost,wcslib,cfitsio))
+builder.add_option("-L%s/lib:%s/lib:%s/lib:%s/lib"%(casacore,boost,wcslib,cfitsio))
+
+builder.build()
