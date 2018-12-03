b4a54b6d577a631678cc39ba521057425f4e1d56
StephenOrd <StephenOrd@csiro.au>
2018-12-03 05:13:35 +0000

    Removed sqlite3

--- 3rdParty/sqlite3/sqlite-autoconf-3200000/build.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from askapdev.rbuild.builders import Autotools as Builder
-
-builder = Builder(pkgname="./sqlite-autoconf-3200000")
-builder.remote_archive = "sqlite-autoconf-3200000.tar.gz"
-builder.nowarnings = True
-builder.build()
