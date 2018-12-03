29b66469e683a8793559e04ed8b3f4031512ee61
StephenOrd <StephenOrd@csiro.au>
2018-12-03 05:05:20 +0000

    removed cfitsio

--- 3rdParty/cfitsio/cfitsio-3.35/build.py
+++
@@ -1,8 +0,0 @@
-from askapdev.rbuild.builders import Autotools as Builder
-
-builder = Builder("cfitsio", buildtargets=['shared'])
-builder.remote_archive = "cfitsio3350.tar.gz"
-builder.nowarnings = True
-builder.add_option("--enable-reentrant")
-builder.add_option("--enable-sse2")
-builder.build()
