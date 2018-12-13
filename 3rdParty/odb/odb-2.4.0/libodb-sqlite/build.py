fecba37cb5bae714e89ede6ba4d0ef3540993564
StephenOrd <StephenOrd@csiro.au>
2018-12-13 05:12:27 +0000

    fixed some nans and some ubuntu specific dependencies

--- 3rdParty/odb/odb-2.4.0/libodb-sqlite/build.py
+++ 3rdParty/odb/odb-2.4.0/libodb-sqlite/build.py
@@ -4,8 +4,6 @@ from askapdev.rbuild.builders import Autotools as Builder
 import askapdev.rbuild.utils as utils
 
 builder = Builder(pkgname="libodb-sqlite-2.4.0")
-platform = utils.get_platform()
-print "####",platform
 # dependencies
 libodb = builder.dep.get_install_path("libodb")
 libsqlite = builder.dep.get_install_path("libsqlite3")
