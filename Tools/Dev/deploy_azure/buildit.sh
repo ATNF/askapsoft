5eb970dc368b957c6c2f8250107de214e784ea7b
StephenOrd <StephenOrd@csiro.au>
2018-12-14 02:13:34 +0000

    added a buildit.sh script to azure

---
+++ Tools/Dev/deploy_azure/buildit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script will run a long-running-job (if it's not already running)
+# and email when it completes.
+lockfile=/var/run/long-job-1.lock
+logfile=$(mktemp)
+errfile=$(mktemp)
+if [[ -f "$lockfile" ]]; then
+    echo "This job is already running." 1>&2
+    exit 1
+else
+    echo $$ > "$lockfile"
+    trap 'rm -f "$lockfile" "$logfile" "$errfile"' EXIT
+fi
+
+echo "running bootstrap.y" >> $logfile
+
+cd ../../../
+python2.7 bootstrap.py -n
+
+echo "done bootstrap" >> $logfile
+
+echo "running rbuild" >> $logfile
+. initaskap.sh
+
+cd Code
+
+rbuild -a
+
+echo "done rbuild" >> $logfile
+
+cat "$logfile" | mailx -s "Job succeeded" stephen.ord@csiro.au
+
+
