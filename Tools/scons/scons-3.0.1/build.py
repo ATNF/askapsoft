651757315e3488243470c25fbf1beb0745c96c94
Stephen Ord <steve.ord@gmail.com>
2018-12-14 13:56:14 +1100

    fixed the filename in the scons builder

--- Tools/scons/scons-3.0.1/build.py
+++ Tools/scons/scons-3.0.1/build.py
@@ -1,6 +1,6 @@
 from askapdev.rbuild.builders import Setuptools as Builder
 
 builder = Builder()
-builder.remote_archive = "scons_3.0.1.tar.gz"
+builder.remote_archive = "scons-3.0.1.tar.gz"
 builder.nowarnings = True
 builder.build()
