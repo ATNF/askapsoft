36cd04a39b805d6fee046228a102c37c131b33fd
Stephen Ord <steve.ord@gmail.com>
2018-12-18 13:51:54 +1100

    tweaked the packager

--- Tools/Dev/deploy_azure/package.sh
+++ Tools/Dev/deploy_azure/package.sh
@@ -2,26 +2,25 @@
 
 to_package=`cat ./bundle.list`
 
-if [ -d ./tmp ] 
-then
-  echo "warn - ./tmp exists"
-else
-  mkdir ./tmp
-fi
+pushd .
 
-if [-d ./tmp/askap_apps ]
+if [-d /tmp/askap_apps ]
 then
-  echo "./tmp/askap_apps directory exists - remove to continue"
+  echo "/tmp/askap_apps directory exists - remove to continue"
 else
-  mkdir ./tmp/askap_apps
+  mkdir /tmp/askap_apps
 fi
 
 for app in $to_package
 do
-  . bundle.sh $app ./tmp/askap_apps
+  . bundle.sh $app /tmp/askap_apps
 done
-cd ./tmp
+cd /tmp
 tar -cvf askap_apps.tar ./askap_apps
 gzip askap_apps.tar
-mv askap_apps.tar.gz ../
+
+popd 
+
+mv /tmp/askap_apps.tar.gz .
+
   
