2da0e250f068ea7d5ecc5275c82233c7001904fa
Stephen Ord <steve.ord@gmail.com>
2018-12-20 11:48:33 +1100

    working small(ish) Dockerfile

---
+++ Tools/Dev/deploy_azure/run-provision.sh
@@ -0,0 +1,11 @@
+apt-get install -y --no-install-recommends libboost-system1.65.1 
+apt-get install -y --no-install-recommends libboost-filesystem1.65.1 
+apt-get install -y --no-install-recommends libboost-regex1.65.1 
+apt-get install -y --no-install-recommends libopenmpi2 
+apt-get install -y --no-install-recommends libboost-program-options1.65.1 
+apt-get install -y --no-install-recommends libboost-thread1.65.1 
+apt-get install -y --no-install-recommends libcfitsio5 
+apt-get install -y --no-install-recommends libgfortran5 
+apt-get install -y --no-install-recommends libgfortran4 
+apt-get install -y --no-install-recommends libzeroc-ice3.7 
+.
