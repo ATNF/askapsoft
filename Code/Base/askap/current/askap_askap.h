c8cfcbe2f7d5a419d2b41a90c783c75117d66e58
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2019-05-06 06:35:16 +1000

    I had to change the lofar find package and the getAskapVersion but it now builds

--- Code/Base/askap/current/askap_askap.h
+++ Code/Base/askap/current/askap_askap.h
@@ -10,10 +10,11 @@
 /// askap namespace
 namespace askap {
   /// @return version of the package
-  std::string getAskapPackageVersion_askap();
+  /// std::string getAskapPackageVersion_askap();
 }
 
   /// The version of the package
-#define ASKAP_PACKAGE_VERSION askap::getAskapPackageVersion_askap()
+// #define ASKAP_PACKAGE_VERSION askap::getAskapPackageVersion_askap()
+#define ASKAP_PACKAGE_VERSION "0.0.0" 
 
 #endif
