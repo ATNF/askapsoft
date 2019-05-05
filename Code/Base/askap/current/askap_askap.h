d824809c89a8cccb7b0db579a7c7b21340daea6a
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2019-05-06 06:35:15 +1000

    ok added the shared build now the link step is failing as I having added all the libraries onto the link line

---
+++ Code/Base/askap/current/askap_askap.h
@@ -0,0 +1,19 @@
+// std include
+#include <string>
+
+#ifndef ASKAP_ASKAP_H
+#define ASKAP_ASKAP_H
+
+  /// The name of the package
+#define ASKAP_PACKAGE_NAME "askap"
+
+/// askap namespace
+namespace askap {
+  /// @return version of the package
+  std::string getAskapPackageVersion_askap();
+}
+
+  /// The version of the package
+#define ASKAP_PACKAGE_VERSION askap::getAskapPackageVersion_askap()
+
+#endif
