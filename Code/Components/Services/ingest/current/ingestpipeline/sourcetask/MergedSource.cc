cae8f44975730e0e8f76da57d406b92ec03fc771
MaximVoronkov <MaximVoronkov@csiro.au>
2019-08-08 06:40:47 +0000

    updated the message (ASKAPSDP-3726)

--- Code/Components/Services/ingest/current/ingestpipeline/sourcetask/MergedSource.cc
+++ Code/Components/Services/ingest/current/ingestpipeline/sourcetask/MergedSource.cc
@@ -312,9 +312,10 @@ VisChunk::ShPtr MergedSource::next(void)
             // this is in direct conflict with how buffers operate, so ultimately correlator/IOCs should be fixed to avoid this condition and
             // not send data with junk time
             ASKAPCHECK((itsMetadata->time() - itsVis->timestamp) < ONE_SECOND * 120, 
-                     "More than two minute discrepancy between metadata and visibility timestamps for stream id="<<itsVisConverter.config().receiverId()<<
+                     "Junk BATs suspected. More than two minute discrepancy between metadata and visibility timestamps for stream id="<<itsVisConverter.config().receiverId()<<
                      ", metadata time: "<<bat2epoch(itsMetadata->time()).getValue()<<
-                                           " visibility time: "<<bat2epoch(itsVis->timestamp).getValue());
+                                           " visibility time: "<<bat2epoch(itsVis->timestamp).getValue()<<
+                     " resync/reprogram of the correlator usually helps to recover until proper cross-checks are done by the IOC");
             //
             if (!logCatchup && lastCatchupVisBAT != itsVis->timestamp) {
                 logCatchup = true;
